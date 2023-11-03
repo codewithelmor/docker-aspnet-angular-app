b1e6c337be9c7ef247cce2a99b557bf9a2eee3e1
Elmor Cabalfin <cabalfinelmor17@gmail.com>
2023-11-03 09:52:21 +0800

    Added database project

---
+++ Database/dbo/Tables/Preference.sql
@@ -0,0 +1,32 @@
+﻿CREATE TABLE [dbo].[Preference] (
+    [Id]              INT                IDENTITY (1, 1) NOT NULL,
+    [Locale]          NVARCHAR (5)       NOT NULL,
+    [IsActive]        BIT                DEFAULT ((1)) NOT NULL,
+    [IsDeleted]       BIT                NOT NULL,
+    [CreatedByUserId] NVARCHAR (450)     NOT NULL,
+    [CreatedDate]     DATETIMEOFFSET (7) DEFAULT (getutcdate()) NOT NULL,
+    [UpdatedByUserId] NVARCHAR (450)     NULL,
+    [UpdatedDate]     DATETIMEOFFSET (7) NULL,
+    [DeletedByUserId] NVARCHAR (450)     NULL,
+    [DeletedDate]     DATETIMEOFFSET (7) NULL,
+    CONSTRAINT [PK_Preference] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Preference_CreatedByUser] FOREIGN KEY ([CreatedByUserId]) REFERENCES [dbo].[AspNetUsers] ([Id]),
+    CONSTRAINT [FK_Preference_DeletedByUser] FOREIGN KEY ([DeletedByUserId]) REFERENCES [dbo].[AspNetUsers] ([Id]),
+    CONSTRAINT [FK_Preference_UpdatedByUser] FOREIGN KEY ([UpdatedByUserId]) REFERENCES [dbo].[AspNetUsers] ([Id])
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Preference_UpdatedByUserId]
+    ON [dbo].[Preference]([UpdatedByUserId] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Preference_DeletedByUserId]
+    ON [dbo].[Preference]([DeletedByUserId] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Preference_CreatedByUserId]
+    ON [dbo].[Preference]([CreatedByUserId] ASC);
+
