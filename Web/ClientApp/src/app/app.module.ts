ec3eb1cb4af42c5b8c23ba3e19fa295cc32db028
Elmor Cabalfin <cabalfinelmor17@gmail.com>
2023-11-03 17:40:09 +0800

    Organized imports in app module

--- Web/ClientApp/src/app/app.module.ts
+++ Web/ClientApp/src/app/app.module.ts
@@ -1,30 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import HomeComponent from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ApiAuthorizationModule } from 'src/api-authorization/api-authorization.module';
 import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
+import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import HomeComponent from './home/home.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
-import { TranslationService } from '../app/shared/services/translation.service';
 import { JwtModule } from '@auth0/angular-jwt';
+import { TranslationService } from '../app/shared/services/translation.service';
 import { environment } from '../environments/environment';
-import { AccountService } from './shared/services/account.service';
-import { ApplicationComponent } from './application/application.component';
-import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
-import { ApplicantGuard } from './shared/auth-guards/applicant-guard';
-import { NotfoundComponent } from './notfound/notfound.component';
 import { AdminComponent } from './admin/admin.component';
+import { ApplicationComponent } from './application/application.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 import { AdminGuard } from './shared/auth-guards/admin-guard';
+import { ApplicantGuard } from './shared/auth-guards/applicant-guard';
+import { AccountService } from './shared/services/account.service';
+import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/');
